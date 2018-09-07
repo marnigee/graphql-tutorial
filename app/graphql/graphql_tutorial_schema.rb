0cd01e1723c6fa5d2dd1e95c948df940e179fd42
Marni Goltsman <marnigee@gmail.com>
2018-09-07 16:48:34 -0400

    Get rid of unnecessary files and code

--- app/graphql/graphql_tutorial_schema.rb
+++ app/graphql/graphql_tutorial_schema.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-# GraphqlTutorialSchema = GraphQL::Schema.define do
-#   mutation(Types::MutationType)
-#   query(Types::QueryType)
-# end
 class GraphqlTutorialSchema < GraphQL::Schema
   query Types::Query
 end
--- app/graphql/types/link_type.rb
+++
@@ -1,7 +0,0 @@
-# Types::LinkType = GraphQL::ObjectType.define do
-#   name 'Link'
-#
-#   field :id, !types.ID
-#   field :url, !types.String
-#   field :description, !types.String
-# end
